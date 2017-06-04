35f2f33ae38dcb2b033d33beba8a9897136d9fb4
Bernd Bestel <bernd@berrnd.de>
2017-06-04 18:39:05 +0200

    Fix build includes

--- build.bat
+++ build.bat
@@ -7,5 +7,6 @@ mkdir "%releasePath%"
 for /f "tokens=*" %%a in ('type version.txt') do set version=%%a
 
 del "%releasePath%\grocy_%version%.zip"
-"build_tools\7za.exe" a -r "%releasePath%\grocy_%version%.zip" "%projectPath%\*" -xr!.* -xr!build_tools -xr!build.bat -xr!composer.json -xr!composer.lock -xr!composer.phar -xr!grocy.phpproj -xr!grocy.phpproj.user -xr!grocy.sln -xr!bower.json
-"build_tools\7za.exe" d "%releasePath%\grocy_%version%.zip" data\*.*
+"build_tools\7za.exe" a -r "%releasePath%\grocy_%version%.zip" "%projectPath%\*" -xr!.* -xr!build_tools -xr!build.bat -xr!composer.json -xr!composer.lock -xr!composer.phar -xr!grocy.phpproj -xr!grocy.phpproj.user -xr!grocy.sln -xr!bower.json -xr!publication_assets 
+"build_tools\7za.exe" a -r "%releasePath%\grocy_%version%.zip" "%projectPath%\.htaccess"
+"build_tools\7za.exe" d "%releasePath%\grocy_%version%.zip" data\*.* data\sessions
