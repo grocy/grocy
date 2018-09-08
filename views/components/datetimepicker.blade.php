a9b696f41c923cb09fd9e1dbe30f9238fd7508bc
Bernd Bestel <bernd@berrnd.de>
2018-09-08 08:36:45 +0200

    Fixed datetimepicker (this closes #43)

--- views/components/datetimepicker.blade.php
+++ views/components/datetimepicker.blade.php
@@ -4,15 +4,17 @@
 
 <div class="form-group">
 	<label for="{{ $id }}">{{ $L($label) }}  <span class="small text-muted"><time id="datetimepicker-timeago" class="timeago timeago-contextual"></time>@if(!empty($hint))<br>{{ $L($hint) }}@endif</span></label>
-	<div class="input-group date datetimepicker @if(!empty($additionalCssClasses)){{ $additionalCssClasses }}@endif" id="{{ $id }}" data-target-input="nearest">
-		<input type="text" required class="form-control datetimepicker-input"
-			data-target="#{{ $id }}" data-format="{{ $format }}"
-			data-init-with-now="{{ BoolToString($initWithNow) }}"
-			data-limit-end-to-now="{{ BoolToString($limitEndToNow) }}"
-			data-limit-start-to-now="{{ BoolToString($limitStartToNow) }}"
-			data-next-input-selector="{{ $nextInputSelector }}" />
-		<div class="input-group-append" data-target="#{{ $id }}" data-toggle="datetimepicker">
-			<div class="input-group-text"><i class="fas fa-calendar"></i></div>
+	<div class="input-group">
+		<div class="input-group date datetimepicker @if(!empty($additionalCssClasses)){{ $additionalCssClasses }}@endif" id="{{ $id }}" data-target-input="nearest">
+			<input type="text" required class="form-control datetimepicker-input"
+				data-target="#{{ $id }}" data-format="{{ $format }}"
+				data-init-with-now="{{ BoolToString($initWithNow) }}"
+				data-limit-end-to-now="{{ BoolToString($limitEndToNow) }}"
+				data-limit-start-to-now="{{ BoolToString($limitStartToNow) }}"
+				data-next-input-selector="{{ $nextInputSelector }}" />
+			<div class="input-group-append" data-target="#{{ $id }}" data-toggle="datetimepicker">
+				<div class="input-group-text"><i class="fas fa-calendar"></i></div>
+			</div>
 		</div>
 		@if(isset($shortcutValue) && isset($shortcutLabel))
 		<div class="form-check w-100">
