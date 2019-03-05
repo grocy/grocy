c813a6500dfd656668387529aee6d2f74b052a8a
Bernd Bestel <bernd@berrnd.de>
2019-03-05 21:11:15 +0100

    Removed embedded binary tools - not needed to be in the repo here

--- .devtools/create_release_package.bat
+++ .devtools/create_release_package.bat
@@ -6,11 +6,11 @@ set releasePath=%projectPath%\.release
 mkdir "%releasePath%"
 
 copy "%projectPath%\version.json" versiontemp.json
-for /f "tokens=*" %%a in ('tools\jq.exe .Version versiontemp.json --raw-output') do set version=%%a
+for /f "tokens=*" %%a in ('jq .Version versiontemp.json --raw-output') do set version=%%a
 del versiontemp.json
 
 del "%releasePath%\grocy_%version%.zip"
-"tools\7za.exe" a -r "%releasePath%\grocy_%version%.zip" "%projectPath%\*" -xr!.* -xr!build.bat -xr!composer.json -xr!composer.lock -xr!package.json -xr!yarn.lock -xr!publication_assets
-"tools\7za.exe" a "%releasePath%\grocy_%version%.zip" "%projectPath%\public\.htaccess"
-"tools\7za.exe" rn "%releasePath%\grocy_%version%.zip" .htaccess public\.htaccess
-"tools\7za.exe" d "%releasePath%\grocy_%version%.zip" data\*.* data\storage data\viewcache\*
+7za a -r "%releasePath%\grocy_%version%.zip" "%projectPath%\*" -xr!.* -xr!build.bat -xr!composer.json -xr!composer.lock -xr!package.json -xr!yarn.lock -xr!publication_assets
+7za a "%releasePath%\grocy_%version%.zip" "%projectPath%\public\.htaccess"
+7za rn "%releasePath%\grocy_%version%.zip" .htaccess public\.htaccess
+7za d "%releasePath%\grocy_%version%.zip" data\*.* data\storage data\viewcache\*
