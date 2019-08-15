257dd644aa23649efd1b9a34f50dfa9d281e6bb1
Bernd Bestel <bernd@berrnd.de>
2019-08-15 14:05:33 +0200

    Render checkbox Userfields in tables as checkmark (closes #326)

--- views/components/userfields_tbody.blade.php
+++ views/components/userfields_tbody.blade.php
@@ -4,7 +4,15 @@
 
 @if($userfield->show_as_column_in_tables == 1)
 	@php $userfieldObject = FindObjectInArrayByPropertyValue($userfieldValues, 'name', $userfield->name) @endphp
-	<td>@if($userfieldObject !== null){{ $userfieldObject->value }}@endif</td>
+	<td>
+	@if($userfieldObject !== null)
+		@if($userfield->type == \Grocy\Services\UserfieldsService::USERFIELD_TYPE_CHECKBOX)
+		@if($userfieldObject->value == 1)<i class="fas fa-check"></i>@endif
+		@else
+		{{ $userfieldObject->value }}
+		@endif
+	@endif
+	</td>
 @endif
 
 @endforeach
