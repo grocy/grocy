fa7a5a5702a0b46b24fecfce6b6b613a3b6d3c3d
Dillan Mills <dillan.mills@gmail.com>
2019-09-24 18:49:33 -0700

    fixed barcode button on iOS and made nicer on the desktop

--- views/components/barcodescanner.blade.php
+++ views/components/barcodescanner.blade.php
@@ -15,7 +15,8 @@
 			right: 0;
 			margin-top: 4px;
 			margin-right: 5px;
-			z-index: 1000;
+		        z-index: 1000;
+                        cursor: pointer;
 		}
 
 		.combobox-container #barcodescanner-start-button {
