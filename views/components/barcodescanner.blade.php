ec7642990c2a9f70b0abdaa1730448ffc0ceb6f4
Dillan Mills <dillan.mills@gmail.com>
2019-09-24 18:56:34 -0700

    fixed indentation

--- views/components/barcodescanner.blade.php
+++ views/components/barcodescanner.blade.php
@@ -15,8 +15,8 @@
 			right: 0;
 			margin-top: 4px;
 			margin-right: 5px;
-		        z-index: 1000;
-                        cursor: pointer;
+		    z-index: 1000;
+			cursor: pointer;
 		}
 
 		.combobox-container #barcodescanner-start-button {
