a70df0d0fea963203ec4a00a918dee8727cceb3f
Marc Ole Bulling <Marc-Ole@gmx.de>
2019-10-15 08:22:21 +0200

    Made GET parameter case insensitive

--- public/viewjs/productform.js
+++ public/viewjs/productform.js
@@ -70,7 +70,7 @@
 						/* If the GET parameter "closeAfterCreation" was passed to the form, it will
                                                    automatically close after creating the product. This only works if the window
                                                    was opened with javascript */
-						if (window.location.href.includes("closeAfterCreation"))
+						if (window.location.href.toLowerCase().includes("closeaftercreation"))
                                                 {
                                                         window.close();
                                                 }
