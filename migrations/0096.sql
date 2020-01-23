6663551a66df9f19f569ba42201617cf3968960c
Bernd Bestel <bernd@berrnd.de>
2020-01-23 21:01:35 +0100

    A day can have multiple meal plan notes (references #477)

--- migrations/0096.sql
+++ migrations/0096.sql
@@ -2,7 +2,5 @@ CREATE TABLE meal_plan_notes (
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	day DATE NOT NULL,
 	note TEXT,
-	row_created_timestamp DATETIME DEFAULT (datetime('now', 'localtime')),
-
-	UNIQUE(day)
+	row_created_timestamp DATETIME DEFAULT (datetime('now', 'localtime'))
 );
