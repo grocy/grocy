0d38b0a038a339b8950418778aca8cc792ea8149
James Addison <jay@jp-hosting.net>
2020-03-22 12:31:02 +0000

    Simplify GitHub release asset filenaming

--- .devtools/create_release_package.bat
+++ .devtools/create_release_package.bat
@@ -5,12 +5,8 @@ set projectPath=%projectPath%\..
 set releasePath=%projectPath%\.release
 mkdir "%releasePath%"
 
-copy "%projectPath%\version.json" versiontemp.json
-for /f "tokens=*" %%a in ('jq .Version versiontemp.json --raw-output') do set version=%%a
-del versiontemp.json
-
-del "%releasePath%\grocy_%version%.zip"
-7za a -r "%releasePath%\grocy_%version%.zip" "%projectPath%\*" -xr!.* -xr!build.bat -xr!composer.json -xr!composer.lock -xr!package.json -xr!yarn.lock -xr!publication_assets
-7za a "%releasePath%\grocy_%version%.zip" "%projectPath%\public\.htaccess"
-7za rn "%releasePath%\grocy_%version%.zip" .htaccess public\.htaccess
-7za d "%releasePath%\grocy_%version%.zip" data\*.* data\storage data\viewcache\*
+del "%releasePath%\grocy.zip"
+7za a -r "%releasePath%\grocy.zip" "%projectPath%\*" -xr!.* -xr!build.bat -xr!composer.json -xr!composer.lock -xr!package.json -xr!yarn.lock -xr!publication_assets
+7za a "%releasePath%\grocy.zip" "%projectPath%\public\.htaccess"
+7za rn "%releasePath%\grocy.zip" .htaccess public\.htaccess
+7za d "%releasePath%\grocy.zip" data\*.* data\storage data\viewcache\*
