375865d80e54f71ee355bb3bd8f00d04a11cd6f3
Bernd Bestel <bernd@berrnd.de>
2020-03-22 18:02:19 +0100

    Changelog and some small changes for #639

--- public/index.php
+++ public/index.php
@@ -2,12 +2,13 @@
 
 require_once __DIR__ . '/../helpers/PrerequisiteChecker.php';
 
-try {
+try
+{
 	(new PrerequisiteChecker)->checkRequirements();
-} catch (ERequirementNotMet $e) {
-    die("Unable to run grocy: " . $e->getMessage());
+}
+catch (ERequirementNotMet $ex)
+{
+    die('Unable to run grocy: ' . $ex->getMessage());
 }
 
 require_once __DIR__ . '/../app.php';
-
-?>
