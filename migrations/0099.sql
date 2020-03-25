a45317aea1f834aee05c51429e5eae2331041ac8
Immae <immae@users.noreply.github.com>
2020-03-25 19:34:56 +0100

    Add shopping location for price tracking (#658)

---
+++ migrations/0099.sql
@@ -0,0 +1,12 @@
+CREATE TABLE shopping_locations (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	name TEXT NOT NULL UNIQUE,
+	description TEXT
+	row_created_timestamp DATETIME DEFAULT (datetime('now', 'localtime'))
+);
+
+ALTER TABLE stock_log
+ADD shopping_location_id INTEGER;
+
+ALTER TABLE stock
+ADD shopping_location_id INTEGER;
