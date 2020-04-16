70a1704bc2d9c050a91d4cdedc69251f4470ca8e
Bernd Bestel <bernd@berrnd.de>
2020-04-16 18:12:50 +0200

    Pulled translations from Transifex

--- .devtools/transifex_download.bat
+++ .devtools/transifex_download.bat
@@ -1,4 +1,9 @@
 pushd ..
 tx pull --all --minimum-perc=80
 tx pull --language en_GB
+copy /Y localization\en\userfield_types.po localization\en_GB\userfield_types.po
+copy /Y localization\en\stock_transaction_types.po localization\en_GB\stock_transaction_types.po
+copy /Y localization\en\component_translations.po localization\en_GB\component_translations.po
+copy /Y localization\en\chore_period_types.po localization\en_GB\chore_period_types.po
+copy /Y localization\en\chore_assignment_types.po localization\en_GB\chore_assignment_types.po
 popd
