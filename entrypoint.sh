f94d7a664a084fc38883a6c4c11eb016004cca7a
Julien Stebenne <julien.stebenne@gmail.com>
2020-07-03 01:10:39 -0400

    Add arguments to start-dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+shopt -s extglob
 APACHE_DOCUMENT_ROOT="/var/www/html/public"
 
 sed -ri -e "s!/var/www/html!${APACHE_DOCUMENT_ROOT}!g" /etc/apache2/sites-available/*.conf
 sed -ri -e "s!/var/www/!${APACHE_DOCUMENT_ROOT}!g" /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
+
+if [ "$CLEAN" = "1" ]; then
+  echo "Doing cleanup of temporary files"
+  rm -rf /var/www/html/data/storage
+  cd /var/www/html/data/viewcache && rm *!(".gitkeep")
+  rm -rf /var/www/html/vendor
+  rm -rf /var/www/html/public/node_modules
+fi
 
 composer install
 yarn install
--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DATA_UID=$UID
+CLEAN=
+
+while [ "$1" != "" ]; do
+  case $1 in
+    -u | --uid )      shift       # Override the UID override passed for www-data
+                      DATA_UID=$1
+                      ;;
+    -c | --clean )    shift       # Delete all cache and vendor files
+                      CLEAN=1
+                      ;;
+    * )               usage
+                      exit 1
+  esac
+  shift
+done
 
 docker build -t grocy:dev -f ./Dockerfile.dev .
-docker run -p 8000:80 -v $DIR:/var/www/html -e DATA_UID=$UID grocy:dev
+docker run -p 8000:80 -v $DIR:/var/www/html -e DATA_UID=$DATA_UID -e CLEAN=$CLEAN grocy:dev
