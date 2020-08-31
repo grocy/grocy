ad4f8a19af7bfdd4ea975e9b6fe5a48fa74eb3fe
Bernd Bestel <bernd@berrnd.de>
2020-08-31 22:13:02 +0200

    Removed the barcodes column from the products list as we don't have that field there anymore

--- views/products.blade.php
+++ views/products.blade.php
@@ -74,7 +74,6 @@
 					<th>{{ $__t('QU stock') }}</th>
 					<th>{{ $__t('QU factor') }}</th>
 					<th>{{ $__t('Product group') }}</th>
-					<th>{{ $__t('Barcode(s)') }}</th>
 
 					@include('components.userfields_thead', array(
 					'userfields' => $userfields
@@ -117,9 +116,6 @@
 					</td>
 					<td>
 						@if(!empty($product->product_group_id)) {{ FindObjectInArrayByPropertyValue($productGroups, 'id', $product->product_group_id)->name }} @endif
-					</td>
-					<td>
-						{{ $product->barcode }}
 					</td>
 
 					@include('components.userfields_tbody', array(
