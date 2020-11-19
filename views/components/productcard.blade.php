8ec7e9923c7d502e17799463c0915754e407e0bb
Bernd Bestel <bernd@berrnd.de>
2020-11-19 18:41:09 +0100

    Fixed productcard aggregated amount was in wrong line

--- views/components/productcard.blade.php
+++ views/components/productcard.blade.php
@@ -39,13 +39,13 @@
 		<strong>{{ $__t('Stock amount') }}:</strong> <span id="productcard-product-stock-amount"
 			class="locale-number locale-number-quantity-amount"></span> <span id="productcard-product-stock-qu-name"></span>
 		<span id="productcard-product-stock-opened-amount"
-			class="small font-italic locale-number locale-number-quantity-amount"></span><br>
-		<strong>{{ $__t('Stock value') }}:</strong> <span id="productcard-product-stock-value"
-			class="locale-number locale-number-currency"></span>
+			class="small font-italic locale-number locale-number-quantity-amount"></span>
 		<span id="productcard-aggregated-amounts"
 			class="pl-2 text-secondary d-none"><i class="fas fa-custom-sigma-sign"></i> <span id="productcard-product-stock-amount-aggregated"
 				class="locale-number locale-number-quantity-amount"></span> <span id="productcard-product-stock-qu-name-aggregated"></span> <span id="productcard-product-stock-opened-amount-aggregated locale-number locale-number-quantity-amount"
 				class="small font-italic"></span></span><br>
+		<strong>{{ $__t('Stock value') }}:</strong> <span id="productcard-product-stock-value"
+			class="locale-number locale-number-currency"></span><br>
 		@if(GROCY_FEATURE_FLAG_STOCK_LOCATION_TRACKING)<strong>{{ $__t('Default location') }}:</strong> <span id="productcard-product-location"></span><br>@endif
 		<strong>{{ $__t('Last purchased') }}:</strong> <span id="productcard-product-last-purchased"></span> <time id="productcard-product-last-purchased-timeago"
 			class="timeago timeago-contextual"></time><br>
