97b95803b865d4cf2b06834b8377f8b0765a958a
Bernd Bestel <bernd@berrnd.de>
2020-12-23 11:54:39 +0100

    Fix potential problem (again) when running/upgrading grocy v3.0.0 with SQLite > 3.8.3 < 3.9.0 (references #1209)

--- migrations/0105.sql
+++ migrations/0105.sql
@@ -72,7 +72,7 @@ FROM (
         FROM stock_current
         WHERE best_before_date IS NOT NULL
         UNION
-        SELECT m.id, 0, 0, 0, null, 0, 0, 0, p.due_type
+        SELECT m.id, 0, 0, 0, null, 0, 0, 0
         FROM stock_missing_products m
 		JOIN products p
 			ON m.id = p.id
