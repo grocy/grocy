f47ca963ab4f4f49bdadc1d4b66dd7ebb1868795
Bernd Bestel <bernd@berrnd.de>
2021-07-23 16:37:45 +0200

    Fixed duplicate barcode migration error (fixes #1546)

--- migrations/0128.sql
+++ migrations/0128.sql
@@ -1,12 +1,18 @@
 -- Duplicate product barcodes were most probably not created on purpose,
 -- so just keep the newer one for any duplicates
 DELETE FROM product_barcodes
-WHERE id IN (
-	SELECT MIN(id)
+WHERE barcode IN (
+	SELECT barcode
 	FROM product_barcodes
 	GROUP BY barcode
 	HAVING COUNT(*) > 1
-);
+	)
+	AND id NOT IN (
+		SELECT MIN(id)
+		FROM product_barcodes
+		GROUP BY barcode
+		HAVING COUNT(*) > 1
+	);
 
 CREATE UNIQUE INDEX ix_product_barcodes ON product_barcodes (
 	barcode
