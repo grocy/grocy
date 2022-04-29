9c1fd176c01fcbe20c4e1cf6845b807828c9d42e
Bernd Bestel <bernd@berrnd.de>
2022-04-29 11:46:50 +0200

    Fixed `shopping_list_to_stock_workflow_auto_submit_when_prefilled` user settings control initialization (fixes #1875)

--- public/viewjs/shoppinglistsettings.js
+++ public/viewjs/shoppinglistsettings.js
@@ -1,6 +1,6 @@
 ﻿if (BoolVal(Grocy.UserSettings.shopping_list_to_stock_workflow_auto_submit_when_prefilled))
 {
-	$("#shopping-list-to-stock-workflow-auto-submit-when-prefilled").prop("checked", true);
+	$("#shopping_list_to_stock_workflow_auto_submit_when_prefilled").prop("checked", true);
 }
 
 if (BoolVal(Grocy.UserSettings.shopping_list_show_calendar))
