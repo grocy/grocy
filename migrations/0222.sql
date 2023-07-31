e946ec79d59f18b47b0ca3392348f4a677832fcd
Bernd Bestel <bernd@berrnd.de>
2023-07-31 18:27:53 +0200

    Fixed typo

--- migrations/0222.sql
+++ migrations/0222.sql
@@ -1,10 +1,10 @@
-CREATE INDEX ix_stock_log_performance1 on stock_log (
+CREATE INDEX ix_stock_log_performance1 ON stock_log (
 	stock_id,
 	transaction_type,
 	amount
 );
 
-CREATE INDEX ix_stock_log_performance2 on stock_log (
+CREATE INDEX ix_stock_log_performance2 ON stock_log (
 	product_id,
 	best_before_date,
 	purchased_date,
