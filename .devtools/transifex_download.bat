de881eb78ea7fd322574f46cd1a43541cc3d8efe
Bernd Bestel <bernd@berrnd.de>
2025-01-23 19:57:55 +0100

    Fixed missing en strings

--- .devtools/transifex_download.bat
+++ .devtools/transifex_download.bat
@@ -1,9 +1,11 @@
 pushd ..
+
 for /d %%d in (localization\*) do (
 	if %%~nxd neq en (
 		tx pull --languages %%~nxd --force
 	)
 )
+
 copy /Y localization\en\userfield_types.po localization\en_GB\userfield_types.po
 copy /Y localization\en\stock_transaction_types.po localization\en_GB\stock_transaction_types.po
 copy /Y localization\en\component_translations.po localization\en_GB\component_translations.po
@@ -11,4 +13,5 @@ copy /Y localization\en\chore_period_types.po localization\en_GB\chore_period_ty
 copy /Y localization\en\chore_assignment_types.po localization\en_GB\chore_assignment_types.po
 copy /Y localization\en\permissions.po localization\en_GB\permissions.po
 copy /Y localization\en\locales.po localization\en_GB\locales.po
+
 popd
