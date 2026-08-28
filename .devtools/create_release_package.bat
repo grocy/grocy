79db4bfbe7b78eee25cbc9141d9791e22cd72b4e
Bernd Bestel <bernd@berrnd.de>
2026-08-28 17:33:18 +0200

    Prepared next release

--- .devtools/create_release_package.bat
+++ .devtools/create_release_package.bat
@@ -10,9 +10,9 @@ for /f "tokens=*" %%a in ('jq .Version versiontemp.json --raw-output') do set ve
 del versiontemp.json
 
 del "%releasePath%\grocy_%version%.zip"
-7za a -r "%releasePath%\grocy_%version%.zip" "%projectPath%\*" -xr!.* -xr!build.bat -xr!composer.json -xr!composer.lock -xr!package.json -xr!yarn.lock -xr!docs -xr!test -x!public\packages\@fortawesome\fontawesome-free\js -x!public\packages\@fortawesome\fontawesome-free\less -x!public\packages\@fortawesome\fontawesome-free\metadata -x!public\packages\@fortawesome\fontawesome-free\scss -x!public\packages\@fortawesome\fontawesome-free\sprites -x!public\packages\@fortawesome\fontawesome-free\svgs
-7za a "%releasePath%\grocy_%version%.zip" "%projectPath%\public\.htaccess"
-7za rn "%releasePath%\grocy_%version%.zip" .htaccess public\.htaccess
-7za d "%releasePath%\grocy_%version%.zip" data\*.* data\storage data\viewcache\* changelog\__TEMPLATE.md
-7za a "%releasePath%\grocy_%version%.zip" "%projectPath%\data\.htaccess"
-7za rn "%releasePath%\grocy_%version%.zip" .htaccess data\.htaccess
+7z a -r "%releasePath%\grocy_%version%.zip" "%projectPath%\*" -xr!.* -xr!build.bat -xr!composer.json -xr!composer.lock -xr!package.json -xr!yarn.lock -xr!docs -xr!test -x!public\packages\@fortawesome\fontawesome-free\js -x!public\packages\@fortawesome\fontawesome-free\less -x!public\packages\@fortawesome\fontawesome-free\metadata -x!public\packages\@fortawesome\fontawesome-free\scss -x!public\packages\@fortawesome\fontawesome-free\sprites -x!public\packages\@fortawesome\fontawesome-free\svgs
+7z a "%releasePath%\grocy_%version%.zip" "%projectPath%\public\.htaccess"
+7z rn "%releasePath%\grocy_%version%.zip" .htaccess public\.htaccess
+7z d "%releasePath%\grocy_%version%.zip" data\*.* data\storage data\viewcache\* changelog\__TEMPLATE.md
+7z a "%releasePath%\grocy_%version%.zip" "%projectPath%\data\.htaccess"
+7z rn "%releasePath%\grocy_%version%.zip" .htaccess data\.htaccess
